e0a8a4b2046256ea02ee3234e286423231717276
Fizal Sarif <fizal.sarif@gmail.com>
2020-02-28 00:26:29 -0500

    finished base

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import sum from './sum';
-import axios from 'axios';
+import EmblaCarousel from 'embla-carousel';
 
+const embla = EmblaCarousel(document.getElementById('embla'),{
+  // contains slides to the carousel viewport to prevent excessive scrolling at the beginning or the end
+  containScroll: true,
 
-console.log(sum(1,2));
+  // infinite loop
+  loop: true,
+});
+
+setInterval(()=>{
+  embla.scrollNext();
+}, 4000);
+
 
 const main = async () => {
-  const res = await axios.get('https://fizal.me/pokeapi/api/v2/name/bulbasaur.json');
-  console.log(res.data);
+
 }
 
 main();
